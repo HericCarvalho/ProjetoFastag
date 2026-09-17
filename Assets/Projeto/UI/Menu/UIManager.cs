82928be38875b06d03395c6487dcb11852200d80
Gabriel Torres <biel.rodrigues7@gmail.com>
2026-09-17 19:31:40 -0300

    UI 1.3
    UI começando a ficar funcional
    
    boneco anda pela blocagem e não fica mais dentro do chão

--- Assets/Projeto/UI/Menu/UIManager.cs
+++ Assets/Projeto/UI/Menu/UIManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
@@ -10,7 +11,7 @@ public class UIManager : MonoBehaviour
     Button Jogar;
     Button Options;
     Button Sair;
-    Button Partidinha;
+    Button Partida;
     Button Criar;
 
 
@@ -21,14 +22,48 @@ public class UIManager : MonoBehaviour
         //Telas
         telaInicial = root.Q<VisualElement>("TelaInicial");
         telaMenu = root.Q<VisualElement>("TelaMenu");
-        telaSelect = root.Q<VisualElement>("TelaSlect");
+        telaSelect = root.Q<VisualElement>("TelaSelect");
 
         //Botoes
         Jogar = root.Q<Button>("play");
         Options = root.Q<Button>("OPTIONS");
         Sair = root.Q<Button>("Quit");
+        Partida = root.Q<Button>("Partidinha");
+        Criar = root.Q<Button>("CriarSala");
+
 
         //Eventos
+        Jogar.clicked += AbrirJogo;
+        //Options.clicked += AbrirOptions;
+        Sair.clicked += SairJogo;
+        Partida.clicked += InicioJogo;
+        Criar.clicked += InicioJogo;
 
+        AbrirInicio();
     }
+
+    void AbrirInicio()
+    {
+        telaInicial.style.display = DisplayStyle.None;
+        telaMenu.style.display = DisplayStyle.Flex;
+        telaSelect.style.display = DisplayStyle.None;
+    }
+
+    void AbrirJogo()
+    {
+        telaInicial.style.display= DisplayStyle.None;
+        telaMenu.style.display = DisplayStyle.None;
+        telaSelect.style.display =DisplayStyle.Flex;
+    }
+
+    void InicioJogo()
+    {
+        SceneManager.LoadScene("Cadeia_Dentro");
+    }
+
+    void SairJogo()
+    {
+        Application.Quit();
+    }
+
 }
